101f2ca886961c171649c634ff89ee6b6561dc11
Yujik <yujika87@gmail.com>
2022-02-13 17:49:35 +0300

    Lesson 45: connect, react-redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import {BrowserRouter, Route} from "react-router-dom";
+import {Route} from "react-router-dom";
 import './App.css';
 import {Dialogs} from './components/Dialogs/Dialogs';
 import {Header} from './components/Header/Header';
 import {Navbar} from './components/Navbar/Navbar';
 import {Profile} from './components/Profile/Profile';
-import {StoreType} from "./redux/state";
 
 
 type AppPropsType = {
@@ -15,22 +14,22 @@ type AppPropsType = {
 
 function App() {
     return (
-            <div className="app-wrapper">
-                <Header/>
-                <Navbar/>
-                <div className="app-wrapper-content">
-                    <Route path='/dialogs'
-                           render={() => <DialogsContainer />}
+        <div className="app-wrapper">
+            <Header/>
+            <Navbar/>
+            <div className="app-wrapper-content">
+                <Route path='/dialogs'
+                       render={() => <DialogsContainer />}
                     />
-                    <Route path='/profile'
-                           render={() => <ProfileContainer />}
+                <Route path='/profile'
+                       render={() => <ProfileContainer />}
                     />
 
                     {/* <Route path='/news' component={News}/>
                     <Route path='/music' component={Music}/>
                     <Route path='/settings' component={Settings}/>*/}
-                </div>
             </div>
+        </div>
     );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-//import {addMessage, addPost, RootStateType, updateNewMessageText, updateNewPostText} from "./redux/state";
-//import store from "./redux/state";
 import store from './redux/redux-store';
 import {BrowserRouter} from "react-router-dom";
 import {Provider} from "react-redux";
