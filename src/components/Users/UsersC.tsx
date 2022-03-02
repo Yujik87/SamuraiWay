c2a84cc1b68fded2ea83f63b78458a3362267c18
Yujik <yujika87@gmail.com>
2022-03-02 22:17:16 +0300

    Lesson 53: class component

---
+++ src/components/Users/UsersC.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import styles from './Users.module.css';
+import axios from "axios";
+import userPhoto from '../../assets/images/avatar_male.png'
+
+class Users extends React.Component{
+    constructor(props: any) {
+        super(props);
+        axios.get('https://social-network.samuraijs.com/api/1.0/users').then((response: any) => {
+            debugger
+            this.props.setUsers(response.data.items);
+        })
+    }
+
+
+    render() {
+        return <div>
+            <button onClick={this.getUsers}>Get users</button>
+            {
+                this.props.users.map((u: any) =>
+                    <div key={u.id}>
+                <span>
+                    <div>
+                        <img src={u.photos.small !== null ? u.photos.small : userPhoto} className={styles.userPhoto}/>
+                    </div>
+                    <div>
+                        {u.followed ?
+                            <button onClick={() => {this.props.unfollow(u.id)}}>Unfollow</button> :
+                            <button onClick={() => {this.props.follow(u.id)}}>Follow</button>
+                        }
+                    </div>
+                </span>
+                        <span>
+                    <span>
+                        <div>{u.fullname}</div>
+                        <div>{u.status}</div>
+                    </span>
+                    <span>
+                        <div>{u.location.country}</div>
+                        <div>{u.location.city}</div>
+                    </span>
+                </span>
+                    </div>)
+            }
+        </div>
+    }
+}
+
+export default Users;
